02b5f12af14c4a14017d9b7e7a3787b1a90e7980
Erik Maanum <erikmaanum@Eriks-MacBook-Air.local>
2020-07-10 10:10:57 -0500

    prettier & eslint, then format all files

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,57 +1,57 @@
 export default {
   widgets: [
     {
-      name: 'sanity-tutorials',
+      name: "sanity-tutorials",
       options: {
-        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
-      }
+        templateRepoId: "sanity-io/sanity-template-gatsby-portfolio",
+      },
     },
-    {name: 'structure-menu'},
+    { name: "structure-menu" },
     {
-      name: 'project-info',
+      name: "project-info",
       options: {
         __experimental_before: [
           {
-            name: 'netlify',
+            name: "netlify",
             options: {
               description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: '',
-                  title: 'Sanity Studio',
-                  name: '',
-                  apiId: ''
+                  buildHookId: "",
+                  title: "Sanity Studio",
+                  name: "",
+                  apiId: "",
                 },
                 {
-                  buildHookId: '',
-                  title: 'Portfolio Website',
-                  name: '',
-                  apiId: ''
-                }
-              ]
-            }
-          }
+                  buildHookId: "",
+                  title: "Portfolio Website",
+                  name: "",
+                  apiId: "",
+                },
+              ],
+            },
+          },
         ],
         data: [
           {
-            title: 'GitHub repo',
-            value: 'https://github.com/elmaanum/sanity-gatsby-wa-portfolio',
-            category: 'Code'
+            title: "GitHub repo",
+            value: "https://github.com/elmaanum/sanity-gatsby-wa-portfolio",
+            category: "Code",
           },
           {
-            title: 'Frontend',
-            value: '',
-            category: 'apps'
-          }
-        ]
-      }
+            title: "Frontend",
+            value: "",
+            category: "apps",
+          },
+        ],
+      },
     },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: "project-users", layout: { height: "auto" } },
     {
-      name: 'document-list',
-      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
-      layout: {width: 'medium'}
-    }
-  ]
-}
+      name: "document-list",
+      options: { title: "Recent projects", order: "_createdAt desc", types: ["sampleProject"] },
+      layout: { width: "medium" },
+    },
+  ],
+};
--- web/src/components/project-preview-grid.js
+++ web/src/components/project-preview-grid.js
@@ -1,16 +1,16 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import ProjectPreview from './project-preview'
+import { Link } from "gatsby";
+import React from "react";
+import ProjectPreview from "./project-preview";
 
-import styles from './project-preview-grid.module.css'
+import styles from "./project-preview-grid.module.css";
 
-function ProjectPreviewGrid (props) {
+function ProjectPreviewGrid(props) {
   return (
     <div className={styles.root}>
       {props.title && <h2 className={styles.headline}>{props.title}</h2>}
       <ul className={styles.grid}>
         {props.nodes &&
-          props.nodes.map(node => (
+          props.nodes.map((node) => (
             <li key={node.id}>
               <ProjectPreview {...node} />
             </li>
@@ -22,13 +22,13 @@ function ProjectPreviewGrid (props) {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 ProjectPreviewGrid.defaultProps = {
-  title: '',
+  title: "",
   nodes: [],
-  browseMoreHref: ''
-}
+  browseMoreHref: "",
+};
 
-export default ProjectPreviewGrid
+export default ProjectPreviewGrid;
