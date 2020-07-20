2683bd94b0133e5a53f755db8b82cb12cae85f56
Maanum <kristofer.maanum@gmail.com>
2020-07-19 19:45:31 -0500

    Added 'priority' to figure schema

--- studio/schemas/objects/figure.js
+++ studio/schemas/objects/figure.js
@@ -18,8 +18,17 @@ export default {
       name: 'alt',
       type: 'string',
       title: 'Alternative text',
-      validation: (Rule) => Rule.error('You have to fill out the alternative text.').required(),
+      validation: Rule => Rule.error('You have to fill out the alternative text.').required(),
       description: 'Important for SEO and accessiblity.',
+      options: {
+        isHighlighted: true,
+      },
+    },
+    {
+      title: 'Priority',
+      name: 'priority',
+      type: 'number',
+      description: 'Set the order to be shown on the site (in the case of multiple images)',
       options: {
         isHighlighted: true,
       },
