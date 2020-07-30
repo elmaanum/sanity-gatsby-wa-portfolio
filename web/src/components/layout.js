f8d34386339137e44019a42a64dc1e725c284916
Maanum <kristofer.maanum@gmail.com>
2020-07-30 10:33:23 -0500

    Created footer element

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -4,20 +4,34 @@ import Header from './header';
 import '../styles/layout.css';
 import styles from './layout.module.css';
 
-const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
-  <>
-    <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
+// const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
+const Layout = ({ children }) => (
+  <div className={styles.pageContainer}>
+    <header style={{ 'background-color': 'dodgerblue', height: '5em' }}>Dummy Header</header>
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
-        <div className={styles.siteInfo}>
-          © {new Date().getFullYear()}, Built with <a href="https://www.sanity.io">Sanity</a> &amp;
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <div className={styles.footerElement}>
+          <span className={styles.contactInfo}>
+            <a target="_blank" href="https://goo.gl/maps/hwHKpixcufFHyZWGA">
+              4159 Heatherton Place, Minnetonka, MN 55345
+            </a>
+          </span>
+          <span className={styles.textDivider}>|</span>
+          <span className={styles.contactInfo}>
+            <a href="tel:612-747-0771">(612) 747-0771</a>
+          </span>
+          <span className={styles.textDivider}>|</span>
+          <span className={styles.contactInfo}>
+            <a href="mailto: info@whittenassociates.com">info@whittenassociates.com</a>
+          </span>
+        </div>
+        <div className={styles.footerElement}>
+          <div className={styles.button}>Contact Us</div>
         </div>
       </div>
     </footer>
-  </>
+  </div>
 );
 
 export default Layout;
