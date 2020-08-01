c77e8f978efba30343dd1ce588b5de984b563c1a
Maanum <kristofer.maanum@gmail.com>
2020-08-01 11:21:11 -0500

    Update layout component to call header and footer components properly

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import Header from './header';
+import Footer from './footer';
 
 import '../styles/layout.css';
 import styles from './layout.module.css';
 
-// const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
-const Layout = ({ children }) => (
-  <div className={styles.pageContainer}>
-    <header style={{ 'background-color': 'dodgerblue', height: '5em' }}>Dummy Header</header>
-    <div className={styles.content}>{children}</div>
-    <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.footerElement}>
-          <span className={styles.contactInfo}>
-            <a target="_blank" href="https://goo.gl/maps/hwHKpixcufFHyZWGA">
-              4159 Heatherton Place, Minnetonka, MN 55345
-            </a>
-          </span>
-          <span className={styles.textDivider}>|</span>
-          <span className={styles.contactInfo}>
-            <a href="tel:612-747-0771">(612) 747-0771</a>
-          </span>
-          <span className={styles.textDivider}>|</span>
-          <span className={styles.contactInfo}>
-            <a href="mailto: info@whittenassociates.com">info@whittenassociates.com</a>
-          </span>
-        </div>
-        <div className={styles.footerElement}>
-          <div className={styles.button}>Contact Us</div>
-        </div>
-      </div>
-    </footer>
-  </div>
-);
+const Layout = ({ children, headline }) => {
+  return (
+    <div className={styles.pageContainer}>
+      <Header headline={headline} />
+      <div className={styles.content}>{children}</div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
