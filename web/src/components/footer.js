9d1fcd866aa620feabc4bbb911b94853c207cecf
Maanum <kristofer.maanum@gmail.com>
2020-08-01 11:19:32 -0500

    Breakout footer into separate component

---
+++ web/src/components/footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerWrapper}>
+        <div className={styles.footerElement}>
+          {/* TODO: Grab org info from GraphQL API when implemented */}
+          <span className={styles.contactInfo}>
+            <a target="_blank" href="https://goo.gl/maps/hwHKpixcufFHyZWGA">
+              4159 Heatherton Place, Minnetonka, MN 55345
+            </a>
+          </span>
+          <span className={styles.textDivider}>|</span>
+          <span className={styles.contactInfo}>
+            <a href="tel:612-747-0771">(612) 747-0771</a>
+          </span>
+          <span className={styles.textDivider}>|</span>
+          <span className={styles.contactInfo}>
+            <a href="mailto: info@whittenassociates.com">info@whittenassociates.com</a>
+          </span>
+        </div>
+        {/* TODO: Link to "Contact Us" modal when complete */}
+        <div className={styles.footerElement}>
+          <div className={styles.button}>Contact Us</div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
