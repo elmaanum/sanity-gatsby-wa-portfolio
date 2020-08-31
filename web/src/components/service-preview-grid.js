fbe379a496cc23549fdb1247d5c140f1541c6b91
Erik Maanum <erikmaanum@Eriks-MacBook-Air.local>
2020-08-30 21:12:47 -0500

    prioritize services

--- web/src/components/service-preview-grid.js
+++ web/src/components/service-preview-grid.js
@@ -3,12 +3,18 @@ import ServicePreview from './service-preview';
 
 import styles from './service-preview-grid.module.css';
 
+const prioritizer = (prioritizedNodes, node) => {
+  prioritizedNodes.splice(node.mainImage.priority - 1, 0, node);
+  return prioritizedNodes;
+};
+const prioritizeServices = (nodes) => nodes.reduce(prioritizer, []);
+
 function ServicePreviewGrid(props) {
   return (
     <div className={styles.root}>
       <ul className={styles.grid}>
         {props.nodes &&
-          props.nodes.map((node) => (
+          prioritizeServices(props.nodes).map((node) => (
             <li key={node.id}>
               <ServicePreview {...node} />
             </li>
