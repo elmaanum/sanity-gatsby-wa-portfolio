190ffb0ea1cbb94ee3162f1b6e5a819485c19a30
Kristofer <kristofer.maanum@gmail.com>
2020-12-20 19:04:57 -0300

    Basic layout

--- web/src/pages/about-us.js
+++ web/src/pages/about-us.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GraphQLErrorList from '../components/graphql-error-list';
 import Layout from '../containers/layout';
+import Container from '../components/container';
 
 const AboutUsPage = (props) => {
   const { errors } = props;
@@ -14,10 +15,40 @@ const AboutUsPage = (props) => {
 
   return (
     <Layout headline="About Us">
-      <div>Here's info about us</div>
-      <div>He's Tim. She's Heather. Together we make the Whitten Associates.</div>
+      <Container>
+        <div>[replace this div with component being created for Services Pages template]
+          Our goal is to create exciting, innovative and successful 
+          communities and developments for our clients. Combining market research 
+          and analysis gives us the unique perspective to provide market responsive solutions.
+          
+          Our background includes extensive developer experience that provides 
+          our clients with a practical and realistic balance to all of their projects. 
+          This combination of strengths and experience gives us the ability to evaluate 
+          land for development as well as continue consulting services through the development 
+          process and successful conclusion.  
+        </div>
+        <div>
+          <div>
+            person text
+          </div>
+          <div>
+            person image
+          </div>
+        </div>
+      </Container>
     </Layout>
   );
 };
 
 export default AboutUsPage;
+
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
