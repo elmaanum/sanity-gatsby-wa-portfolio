f1c601ad47c93c928a762abbc8c094b93f6e4d28
Kristofer <kristofer.maanum@gmail.com>
2020-12-23 19:20:52 -0300

    Update merged service tile component styling

--- web/src/components/service-preview.js
+++ web/src/components/service-preview.js
@@ -4,21 +4,24 @@ import { buildImageObj } from '../lib/helpers';
 import { imageUrlFor } from '../lib/image-url';
 import styles from './service-preview.module.css';
 
+// TODO: Darken background behind white text on Service Cards so the text is visible
+
 function ServicePreview(props) {
   return (
     <div className={styles.leadMediaThumb}>
-      <h3 className={styles.responsiveTitle}>{props.title}</h3>
+      <h3 className={`${styles.responsiveTitle} textH2`}>{props.title}</h3>
       {props.mainImage && props.mainImage.asset && (
         <img
           src={imageUrlFor(buildImageObj(props.mainImage))
             .width(600)
             .height(Math.floor((9 / 16) * 1000))
+            .fit('clip')
             .url()}
           alt={props.mainImage.alt}
         />
       )}
       <Link to={`/service/${props.slug.current}`}>
-        <button className={styles.responsiveButton}>Learn More</button>
+        <button className={`${styles.responsiveButton} button accent textH3`}>Learn More</button>
       </Link>
     </div>
   );
