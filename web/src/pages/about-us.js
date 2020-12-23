fbbaee81a61ce0828d56d819a45b9c90dac73a92
Kristofer <kristofer.maanum@gmail.com>
2020-12-23 00:15:17 -0300

    'About us' page and bio component complete

--- web/src/pages/about-us.js
+++ web/src/pages/about-us.js
@@ -3,6 +3,7 @@ import GraphQLErrorList from '../components/graphql-error-list';
 import Layout from '../containers/layout';
 import Container from '../components/container';
 import PersonInfo from '../components/personInfo';
+import styles from './about-us.module.css';
 
 const AboutUsPage = (props) => {
   const { errors, data } = props;
@@ -17,19 +18,20 @@ const AboutUsPage = (props) => {
   return (
     <Layout headline="About Us">
       <Container>
-        <div>[replace this div with component being created for Services Pages template]
-          Our goal is to create exciting, innovative and successful 
-          communities and developments for our clients. Combining market research 
-          and analysis gives us the unique perspective to provide market responsive solutions.
-          
-          Our background includes extensive developer experience that provides 
-          our clients with a practical and realistic balance to all of their projects. 
-          This combination of strengths and experience gives us the ability to evaluate 
-          land for development as well as continue consulting services through the development 
-          process and successful conclusion.  
-        </div>
         <div>
-          {data.allSanityPerson.nodes.map((personData) => <PersonInfo personData={personData} />)}
+          [replace this div with component being created for Services Pages template] Our goal is to
+          create exciting, innovative and successful communities and developments for our clients.
+          Combining market research and analysis gives us the unique perspective to provide market
+          responsive solutions. Our background includes extensive developer experience that provides
+          our clients with a practical and realistic balance to all of their projects. This
+          combination of strengths and experience gives us the ability to evaluate land for
+          development as well as continue consulting services through the development process and
+          successful conclusion.
+        </div>
+        <div className={styles.personListContainer}>
+          {data.allSanityPerson.nodes.map((personData) => (
+            <PersonInfo personData={personData} key={personData.slug.current} />
+          ))}
         </div>
       </Container>
     </Layout>
@@ -43,6 +45,7 @@ export const query = graphql`
     allSanityPerson {
       nodes {
         name
+        credentials
         image {
           alt
           asset {
@@ -50,7 +53,10 @@ export const query = graphql`
           }
         }
         _rawBio
+        slug {
+          current
+        }
       }
     }
   }
-`
+`;
