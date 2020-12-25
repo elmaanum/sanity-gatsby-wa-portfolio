4a31a9afcd4f47346b8a99a321398c07fcac9cb0
Kristofer <kristofer.maanum@gmail.com>
2020-12-25 15:09:22 -0300

    Portfolio grid with search v1

--- web/src/pages/portfolio.js
+++ web/src/pages/portfolio.js
@@ -1,23 +1,108 @@
 import React from 'react';
-import GraphQLErrorList from '../components/graphql-error-list';
 import Layout from '../containers/layout';
+import styles from './portfolio.module.css';
+import { buildImageObj } from '../lib/helpers';
+import { imageUrlFor } from '../lib/image-url';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
-const PortfolioPage = (props) => {
-  const { errors } = props;
-  if (errors) {
+export const query = graphql`
+  query PortfolioPageQuery {
+    allSanityProject {
+      edges {
+        node {
+          title
+          slug {
+            current
+          }
+          serviceTypes {
+            title
+          }
+          images {
+            _key
+            _type
+            priority
+            crop {
+              top
+              bottom
+              left
+              right
+            }
+            asset {
+              id
+              _id
+            }
+            hotspot {
+              x
+              y
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const createProjectTile = (project) => (
+  <div className={styles.projectTile}>
+    <img
+      className={styles.projectImage}
+      src={imageUrlFor(buildImageObj(project.image)).height(300).width(300).fit('fill').url()}
+    />
+    <div className={`${styles.projectName} textH3`}>{project.title}</div>
+    <div>{project.service}</div>
+  </div>
+);
+
+class PortfolioPage extends React.Component {
+  state = { projectsList: [] };
+
+  componentWillMount = () => {
+    this.allProjects = this.props.data.allSanityProject.edges.map(({ node }) => ({
+      title: node.title,
+      slug: node.slug.current,
+      image: node.images.reduce((max, image) => (max.priority > image.priority ? max : image)),
+      service: node.serviceTypes[0] ? node.serviceTypes[0].title : '',
+    }));
+
+    let serviceSet = new Set(
+      []
+        .concat(...this.props.data.allSanityProject.edges.map((edge) => edge.node.serviceTypes))
+        .map((item) => item.title),
+    );
+    this.serviceArray = ['all', ...serviceSet];
+  };
+
+  _onSelect = (event) => {
+    this.setState({
+      projectsList:
+        event.value == 'all'
+          ? this.allProjects
+          : this.allProjects.filter((project) => project.service == event.value),
+    });
+  };
+
+  render() {
     return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
+      <Layout headline="Portfolio">
+        <div className={styles.selectionLine}>
+          <pre>Show me&nbsp;</pre>
+          <Dropdown
+            className={styles.dropdown}
+            options={this.serviceArray}
+            onChange={this._onSelect}
+            value={'all'}
+          />
+          <pre>&nbsp;projects</pre>
+        </div>
+        <div className={styles.projectsListContainer}>
+          {this.state.projectsList.map((project) => createProjectTile(project))}
+        </div>
       </Layout>
     );
   }
-
-  return (
-    <Layout headline="Portfolio">
-      <div>Here's a Portfolio:</div>
-      <div>[pictures of buildings]</div>
-    </Layout>
-  );
-};
+}
 
 export default PortfolioPage;
