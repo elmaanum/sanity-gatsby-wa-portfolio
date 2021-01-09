399b9833573c01e04dcc6235647bfcd1f1f6d46d
Kristofer Gordon Maanum <kristofer.maanum@gmail.com>
2021-01-08 20:42:40 -0600

    WIP

--- web/src/components/contact-us.js
+++ web/src/components/contact-us.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './contact-us.module.css';
 import iconClose from '../assets/close-button-grey.svg';
 import Modal from '../components/modal.js';
+import { render } from 'react-dom';
+
+const isEmail = (email) =>
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 
 const encode = (data) => {
   return Object.keys(data)
@@ -9,139 +13,182 @@ const encode = (data) => {
     .join('&');
 };
 
-const ContactUs = (props) => {
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-    company: '',
-  });
+// const ContactUs = (props) => {
+//   const [invalidEmail, setInvalidEmail] = useState(false);
+//   const [showModal, setShowModal] = useState(false);
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     message: '',
+//     company: '',
+//   });
 
-  const submitClicked = (event) => {
-    event.preventDefault();
+class ContactUs extends React.Component {
+  state = {
+    invalidEmail: false,
+    showModal: false,
+    formData: {
+      name: '',
+      email: '',
+      message: '',
+      company: '',
+    },
+  };
+
+  submitClicked = (event) => {
+    // event.preventDefault();
+
+    if (!isEmail(this.state.formData.email)) {
+      console.log(`${this.state.formData.email} is not an email`);
+      this.setState({ invalidEmail: true });
+      return;
+    }
 
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...formData }),
+      body: encode({ 'form-name': 'contact', ...this.state.formData }),
     })
       .then(() => alert('Success!'))
       .catch((error) => alert(error));
   };
 
-  const onInputChange = (event) => {
-    event.persist();
-    console.log('old', formData);
-    const updatedFormData = {
-      ...formData,
-      [event.target.name]: event.target.value,
-    };
-    console.log('update to', updatedFormData);
-    setFormData(updatedFormData);
-    console.log('updated', formData);
+  UNSAFE_componentWillUpdate = () => {
+    console.log('will update');
   };
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    console.log('Conventional form submit detected!');
+  handleChange = (e) => {
+    // const freshFormData = {
+    //   ...formData,
+    //   [e.target.name]: e.target.value,
+    // };
+
+    // console.log(freshFormData);
+    // this.setState({ name: event.target.value });
+    e.persist();
+    console.log(e.target.name, e.target.value);
+
+    this.setState((prevState) => ({
+      ...prevState,
+      formData: { [e.target.name]: e.target.value },
+    }));
+
+    // let freshFormData = { ...this.state.formData, [e.target.name]: e.target.value };
+    // setFormData(freshFormData);
+
+    // console.log(`fresh: ${JSON.stringify(freshFormData)}`);
+    // this.setState({ formData: freshFormData });
+
+    if (isEmail(this.state.formData.email)) {
+      this.setState({ invalidEmail: false });
+    }
   };
 
-  const onClickContactUsButton = () => setShowModal(true);
-  const onClickClose = () => setShowModal(false);
+  onClickContactUsButton = () => this.setState({ showModal: true });
+  onClickClose = () => setShowModal(false);
 
-  return (
-    <div>
-      <div onClick={onClickContactUsButton}>{props.children}</div>
-      {showModal && (
-        <Modal
-          style={{
-            width: '80%',
-            height: '75%',
-            maxWidth: '840px',
-            maxHeight: '500px',
-            top: '10%',
-          }}
-        >
-          <div className={styles.mainContainer}>
-            <div className={styles.leftFrame}>
-              <div className={`${styles.leftText} textH2`}>Let's Talk!</div>
-              <div className={`${styles.leftText} textBody`}>
-                We’ve provided planning services to over 43 companies over the last 9 years and are
-                excited to see what we can do for you!
+  render() {
+    const { name, email, message, company } = this.state.formData;
+    return (
+      <div>
+        <div onClick={this.onClickContactUsButton}>{this.props.children}</div>
+        {this.state.showModal && (
+          <Modal
+            style={{
+              width: '80%',
+              height: '75%',
+              maxWidth: '840px',
+              maxHeight: '500px',
+              top: '10%',
+            }}
+          >
+            <div className={styles.mainContainer}>
+              <div className={styles.leftFrame}>
+                <div className={`${styles.leftText} textH2`}>Let's Talk!</div>
+                <div className={`${styles.leftText} textBody`}>
+                  We’ve provided planning services to over 43 companies over the last 9 years and
+                  are excited to see what we can do for you!
+                </div>
+                <div className={`${styles.leftText} textBody`}>
+                  Please fill out the form to the right and we’ll get back to you, or you can
+                  contact us via email or phone below.
+                </div>
+                <div className={`${styles.leftText} textBody`}>
+                  <a className={styles.underline} href="tel:(612) 747-0771">
+                    (612) 747-0771
+                  </a>
+                </div>
+                <div className={`${styles.leftText} textBody`}>
+                  <a className={styles.underline} href="mailto:info@whittenassociates.com">
+                    info@whittenassociates.com
+                  </a>
+                </div>
               </div>
-              <div className={`${styles.leftText} textBody`}>
-                Please fill out the form to the right and we’ll get back to you, or you can contact
-                us via email or phone below.
+              <div className={styles.rightFrame}>
+                <div className={styles.formContainer}>
+                  <form className={styles.formItems} onSubmit={this.submitClicked}>
+                    <div className={styles.formSet}>
+                      <label className="textBody">Name</label>
+                      {console.log(name)}
+                      <input
+                        value={name}
+                        onChange={this.handleChange}
+                        className={styles.inputShort}
+                        type="text"
+                        name="name"
+                      />
+                    </div>
+                    <div className={styles.formSet}>
+                      <label className="textBody">Email*</label>
+                      {/* {console.log(invalidEmail ? '2px solid red' : '')} */}
+                      <input
+                        value={email}
+                        className={styles.inputShort}
+                        style={{ border: this.state.invalidEmail ? '2px solid red' : '' }}
+                        // style={{ border: '2px solid red' }}
+                        type="text"
+                        name="email"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className={styles.formSet}>
+                      <label className="textBody">Company</label>
+                      <input
+                        value={company}
+                        className={styles.inputShort}
+                        type="text"
+                        name="company"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className={styles.formSet}>
+                      <label className="textBody">Message</label>
+                      <textarea
+                        value={message}
+                        className={styles.inputTall}
+                        type="text"
+                        name="message"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <button type="submit" className={`${styles.submitButtom} button accent`}>
+                      Start the Conversation
+                    </button>
+                  </form>
+                </div>
               </div>
-              <div className={`${styles.leftText} textBody`}>
-                <a className={styles.underline} href="tel:(612) 747-0771">
-                  (612) 747-0771
-                </a>
-              </div>
-              <div className={`${styles.leftText} textBody`}>
-                <a className={styles.underline} href="mailto:info@whittenassociates.com">
-                  info@whittenassociates.com
-                </a>
-              </div>
+              <img
+                onClick={this.onClickClose}
+                className={styles.iconClose}
+                src={iconClose}
+                alt={'close'}
+              />
             </div>
-            {console.log('render', formData)}
-            <div className={styles.rightFrame}>
-              <div className={styles.formContainer}>
-                <form className={styles.formItems} onSubmit={onFormSubmit}>
-                  <div className={styles.formSet}>
-                    <label className="textBody">Name</label>
-                    <input
-                      onChange={onInputChange}
-                      className={styles.inputShort}
-                      type="text"
-                      name="name"
-                    />
-                  </div>
-                  <div className={styles.formSet}>
-                    <label className="textBody">Email*</label>
-                    <input
-                      className={styles.inputShort}
-                      type="email"
-                      name="email"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                  <div className={styles.formSet}>
-                    <label className="textBody">Company</label>
-                    <input
-                      className={styles.inputShort}
-                      type="text"
-                      name="company"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                  <div className={styles.formSet}>
-                    <label className="textBody">Message</label>
-                    <textarea
-                      className={styles.inputTall}
-                      type="text"
-                      name="message"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                </form>
-                <button onClick={submitClicked} className={`${styles.submitButtom} button accent`}>
-                  Start the Conversation
-                </button>
-              </div>
-            </div>
-            <img
-              onClick={onClickClose}
-              className={styles.iconClose}
-              src={iconClose}
-              alt={'close'}
-            />
-          </div>
-        </Modal>
-      )}
-    </div>
-  );
-};
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ContactUs;
