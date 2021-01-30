d39a9fb7974a76cc48e836ad8abaa6146e2ac8b6
Maanum <kristofer.maanum@gmail.com>
2021-01-30 14:18:55 -0600

    Create contact-us modal

--- web/src/components/contact-us.js
+++ web/src/components/contact-us.js
@@ -2,57 +2,13 @@ import React, { useState } from 'react';
 import styles from './contact-us.module.css';
 import iconClose from '../assets/close-button-grey.svg';
 import Modal from '../components/modal.js';
-
-const isEmail = (email) =>
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
-
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-};
+import ContactUsForm from './contact-us-form.js';
 
 const ContactUs = (props) => {
-  const [invalidEmail, setInvalidEmail] = useState({});
-  const [showModal, setShowModal] = useState({});
-  const [formData, setFormData] = useState({});
-
-  const submitClicked = (event) => {
-    event.preventDefault();
-    if (!isEmail(formData.email)) {
-      console.log(`${formData.email} is not an email`);
-      setInvalidEmail(true);
-      return;
-    }
-
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', formData }),
-    })
-      .then(() => alert('Success!'))
-      .catch((error) => alert(error));
-  };
-
-  const handleChange = (event) => {
-    const updateObj = {
-      ...formData,
-      [event.target.name]: event.target.value,
-    };
-    console.log(updateObj);
-    // console.log({
-    //   ...formData,
-    //   [event.target.name]: event.target.value,
-    // });
-    setFormData(updateObj);
-
-    if (isEmail(formData.email)) {
-      setInvalidEmail(false);
-    }
-  };
+  const [showModal, setShowModal] = useState(false);
 
   const onClickContactUsButton = () => setShowModal(true);
-  const onClickClose = () => setShowModal(false);
+  const closeModal = () => setShowModal(false);
 
   return (
     <div>
@@ -91,60 +47,10 @@ const ContactUs = (props) => {
             </div>
             <div className={styles.rightFrame}>
               <div className={styles.formContainer}>
-                <form className={styles.formItems} onSubmit={submitClicked}>
-                  <label className={`${styles.formSet} textBody`}>
-                    Name
-                    <input
-                      value={formData.name}
-                      onChange={handleChange}
-                      className={styles.inputShort}
-                      type="text"
-                      name="formName"
-                    />
-                  </label>
-                  <label className={`${styles.formSet} textBody`}>
-                    Email*
-                    <input
-                      value={formData.email}
-                      className={styles.inputShort}
-                      style={{ border: invalidEmail ? '2px solid red' : '' }}
-                      type="text"
-                      name="formEmail"
-                      onChange={handleChange}
-                    />
-                  </label>
-                  <label className={`${styles.formSet} textBody`}>
-                    Company
-                    <input
-                      value={formData.company}
-                      className={styles.inputShort}
-                      type="text"
-                      name="formCompany"
-                      onChange={handleChange}
-                    />
-                  </label>
-                  <label className={`${styles.formSet} textBody`}>
-                    Message
-                    <textarea
-                      value={formData.message}
-                      className={styles.inputTall}
-                      type="text"
-                      name="formMessage"
-                      onChange={handleChange}
-                    />
-                  </label>
-                  <button type="submit" className={`${styles.submitButtom} button accent`}>
-                    Start the Conversation
-                  </button>
-                </form>
+                <ContactUsForm closeModal={closeModal} />
               </div>
             </div>
-            <img
-              onClick={onClickClose}
-              className={styles.iconClose}
-              src={iconClose}
-              alt={'close'}
-            />
+            <img onClick={closeModal} className={styles.iconClose} src={iconClose} alt={'close'} />
           </div>
         </Modal>
       )}
