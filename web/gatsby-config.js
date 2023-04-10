1b856d1edd5394608b84b37930907eb438015870
Maanum <kristofer.maanum@gmail.com>
2023-04-09 20:23:49 -0500

    Remove unused plugin from gatsby config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,7 +11,6 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     {
       resolve: 'gatsby-source-sanity',
